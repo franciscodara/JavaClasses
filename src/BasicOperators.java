7b7086a7777ba3160dfc80252e7a3e4abe4eddce
Francisco Dara <franciscodara@gmail.com>
2020-10-25 17:31:14 -0300

    basic operetors

--- src/BasicOperators.java
+++ src/BasicOperators.java
@@ -2,10 +2,11 @@
  * Operadores unáruios:
  * Increment (++);
  * Decrement (--);
- * Plus (+);
- * Minus (-);
+ * Addition (+);
+ * Subtraction (-);
  * Multiplication (*);
  * Division (/);
+ * Modulus (%);
  * Cast ( () );
  * Not (!).
  */
@@ -14,9 +15,11 @@ public class Opetetors {
     public static void main (String[] args) {
         int number1 = 20;
         int number2 = 10;
+        int number3 = 21;
         System.out.println("A soma dos números 20 e 10 é: " + (number1 + number2));
         System.out.println("A subtração dos números 20 e 10 é: " + (number1 - number2));
         System.out.println("A multiplicação dos números 20 e 10 é: " + (number1 * number2));
         System.out.println("A divisão dos números 20 e 10 é: " + (number1 / number2));
+        System.out.println("O resto da divisão dos números 21 e 10 é: " + (number3 % number2));
     }
 }
